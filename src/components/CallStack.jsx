66f1e22060a36a3cdf3afc4e5e9ae9284d289ff1
DevDolphin7 <DevDolphin7@users.noreply.github.com>
2024-11-28 21:37:47 +0000

    Add CallStack component with test data

---
+++ src/components/CallStack.jsx
@@ -0,0 +1,12 @@
+function CallStack() {
+  return (
+    <section className="call-stack">
+      <div className="call-stack-item">global</div>
+      <div className="call-stack-item">run(3)</div>
+      <div className="call-stack-item">run(2)</div>
+      <div className="call-stack-item">run(1)</div>
+    </section>
+  );
+}
+
+export default CallStack;
