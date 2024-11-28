03ae16aa0537a693dfa1dbf2d8826e1d1f6a36e0
DevDolphin7 <DevDolphin7@users.noreply.github.com>
2024-11-28 21:37:27 +0000

    Basic completion of SidePane component

--- src/components/SidePane.jsx
+++ src/components/SidePane.jsx
@@ -1,6 +1,20 @@
+import CallStack from "./CallStack";
+import RecursiveRunImg from "../assets/Recursive-Run.png";
+
 function SidePane() {
-    return <section className="side-pane-container pane-borders">Button to go here!</section>;
-  }
-  
-  export default SidePane;
-  
+  return (
+    <section className="side-pane-container pane-borders">
+      <section className="code-image">
+        <img
+          src={RecursiveRunImg}
+          alt="A screenshot of a recursive function that counts down the number of kilometers left in a run"
+        />
+      </section>
+      <div className="call-stack-container">
+        <CallStack />
+      </div>
+    </section>
+  );
+}
+
+export default SidePane;
