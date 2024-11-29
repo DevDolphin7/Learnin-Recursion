eab6cd2bd2064349b970aa061f583a9aa7450c43
NCDanOlver <daniel.olver@northcoders.com>
2024-11-29 18:14:21 +0000

    Change prop name and value

--- src/components/MainPane.jsx
+++ src/components/MainPane.jsx
@@ -3,7 +3,7 @@ import ExecutionContextDiagram from "./ExecutionContextDiagram";
 function MainPane() {
   return (
     <section className="main-pane-container pane-borders">
-      <ExecutionContextDiagram levels={10} />
+      <ExecutionContextDiagram level={4} />
     </section>
   );
 }
