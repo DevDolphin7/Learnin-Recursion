5e700c875812aa95844036e1c3be16926d0968bf
NCDanOlver <daniel.olver@northcoders.com>
2024-11-29 18:15:10 +0000

    Make page responsive to insufficiently small devices

--- src/routes/Kata.jsx
+++ src/routes/Kata.jsx
@@ -1,13 +1,35 @@
+import { useState, useEffect } from "react";
 import SidePane from "../components/SidePane";
 import MainPane from "../components/MainPane";
 import UserInterfacePane from "../components/UserInterfacePane";
 
 function Kata() {
+  const [sufficientDimensions, setSufficientDimensions] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setSufficientDimensions(
+        window.innerWidth > 809 && window.innerHeight > 409
+      );
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="page-container">
-      <SidePane />
-      <MainPane />
-      <UserInterfacePane />
+      {sufficientDimensions ? (
+        <>
+          <SidePane />
+          <MainPane />
+          <UserInterfacePane />
+        </>
+      ) : (
+        <h1 style={{ gridColumn: "1 / span 2" }}>
+          The screen dimensions are too small to handle all the recursion!
+        </h1>
+      )}
     </div>
   );
 }
