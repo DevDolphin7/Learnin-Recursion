edf7ff95d05b1a8f4f8013ba4eb86f407a372c36
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-02 16:37:01 +0000

    Add and style UserInterfacePane component

--- src/components/UserInterfacePane.jsx
+++ src/components/UserInterfacePane.jsx
@@ -1,5 +1,27 @@
 function UserInterfacePane() {
-    return <section className="ui-pane-container pane-borders">User Interface stuff to go here!</section>;
-  }
-  
-  export default UserInterfacePane;
+  const stepText = "Hello world, how are you?";
+
+  const handlePrevious = () => {
+    console.log("Previous step");
+  };
+
+  const handleNext = () => {
+    console.log("Next step");
+  };
+
+  return (
+    <section className="ui-pane-container pane-borders">
+      <div className="comment-box">
+        <p>{stepText}</p>
+      </div>
+      <a className="step-arrow" onClick={handlePrevious}>
+        ⬅️
+      </a>
+      <a className="step-arrow" onClick={handleNext}>
+        ➡️
+      </a>
+    </section>
+  );
+}
+
+export default UserInterfacePane;
