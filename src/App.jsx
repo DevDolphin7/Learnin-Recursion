5ae9dafa0a9f0086fea6ee7e5fc6b5b778d2aae4
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-03 20:30:23 +0000

    Add comments for future katas to be added, add stepsJson prop

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Kata from "./routes/Kata";
 
+// Import steps defined in JSON files here
+import run from "./katas/run.json";
+
 function App() {
   return (
     <div className="page-container">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recursive-kata/:kata_id" element={<Kata />} />
+        {/* Add a new route below */}
+        <Route
+          path="/recursive-kata/:kata_id"
+          element={<Kata stepsJson={run} />}
+        />
       </Routes>
     </div>
   );
