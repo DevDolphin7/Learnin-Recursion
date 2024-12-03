61ce6d4a2453b94877bece9fc95c78c3a7be2dfc
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-03 20:51:57 +0000

    Use context and keep thread visible adding new diagram below existing text

--- src/components/ExecutionContextDiagram.jsx
+++ src/components/ExecutionContextDiagram.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import { StepInformationContext } from "../contexts/StepInformation";
 import RecursiveExecutionContextDiagram from "./ExecutionContextDiagram";
 
-function ExecutionContextDiagram({ level }) {
+function ExecutionContextDiagram() {
+  const { diagramInfo } = useContext(StepInformationContext);
+  const { thread, envVars, level } = diagramInfo;
+
   if (level === 0) return;
 
   if (level > 4) {
@@ -8,20 +13,41 @@ function ExecutionContextDiagram({ level }) {
     return;
   }
 
+  const recursiveComponentCall = () => {
+    diagramInfo.level--;
+    return <RecursiveExecutionContextDiagram key={diagramInfo.level} />;
+  };
+
+  const threadContent = (newDiagram = false) => {
+    const formattedThreads = thread.map((lineOfJS, index) => (
+      <p key={index}>{lineOfJS}</p>
+    ));
+    if (newDiagram) {
+      formattedThreads.push(recursiveComponentCall());
+    }
+    return formattedThreads;
+  };
+
   return (
     <section className="execution-context-diagram-container">
       <header className="ecd-header-thread">
         {level === 1 ? "Thread" : null}
       </header>
       <header className="ecd-header-environment-variables">E. V.</header>
+
       {level !== 1 ? (
         <div className="new-ecd-container">
-          <RecursiveExecutionContextDiagram level={level - 1} />
+          {threadContent({ newDiagram: true })}
         </div>
       ) : (
-        <div className="ecd-main-thread">finished</div>
+        <div className="ecd-main-thread">{threadContent()}</div>
       )}
-      <div className="ecd-main-environment-variables">Test</div>
+
+      <div className="ecd-main-environment-variables">
+        {envVars.map((envVar, index) => (
+          <p key={index}>{envVar}</p>
+        ))}
+      </div>
     </section>
   );
 }
