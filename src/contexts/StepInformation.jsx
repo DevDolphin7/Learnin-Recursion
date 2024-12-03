c8f9756070f942f1dffd468ed9510131c5b02a79
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-03 20:35:15 +0000

    Update context to include all the relevant information as states

--- src/contexts/StepInformation.jsx
+++ src/contexts/StepInformation.jsx
@@ -1,32 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const StepInformationContext = createContext();
 
 export function StepInformationProvider(props) {
+  const [stepInfo, setStepInfo] = useState({
+    currentStep: 0,
+    steps: [
+      {
+        text: "",
+        thread: [],
+        envVars: [],
+        callStack: [],
+        level: 1,
+      },
+    ],
+  });
+
+  const { thread, envVars, level } = stepInfo.steps[0];
+  const [diagramInfo, setDiagramInfo] = useState({ thread, envVars, level });
+
+  const { text } = stepInfo.steps[0];
+  const [userInstructions, setUserInstructions] = useState(text);
+
+  const { callStack } = stepInfo.steps[0];
+  const [onCallStack, setOnCallStack] = useState(callStack);
+
   return (
     <StepInformationContext.Provider
       value={{
-        currentStep: 0,
-        steps: [
-          {
-            text: "Hello, world!!!",
-            thread: ["test"],
-            envVars: ["test"],
-            level: 1,
-          },
-          {
-            text: "How are you?",
-            thread: ["Function name yay!"],
-            envVars: ["one = 'hello'"],
-            level: 1,
-          },
-          {
-            text: "Good thanks!!!",
-            thread: ["Function name yay!"],
-            envVars: ["one = 'hello'"],
-            level: 2,
-          },
-        ],
+        stepInfo,
+        setStepInfo,
+        onCallStack,
+        setOnCallStack,
+        diagramInfo,
+        setDiagramInfo,
+        userInstructions,
+        setUserInstructions,
       }}
     >
       {props.children}
