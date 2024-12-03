51cdec3a3abf31a1269803a0074a984f26a44b7a
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-03 20:30:39 +0000

    Remove multiple contexts - all in one now

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { OnCallStackProvider } from "./contexts/OnCallStack";
 import { StepInformationProvider } from "./contexts/StepInformation.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <OnCallStackProvider>
-      <StepInformationProvider>
-        <App />
-      </StepInformationProvider>
-    </OnCallStackProvider>
+    <StepInformationProvider>
+      <App />
+    </StepInformationProvider>
   </BrowserRouter>
 );
