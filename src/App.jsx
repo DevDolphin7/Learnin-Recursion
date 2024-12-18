62ea7e4223d8bf33a236be7e9a4c06fd81b8e467
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-18 14:49:01 +0000

    Iteratively render new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,33 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Kata from "./routes/Kata";
 
-// Import steps defined in JSON files and code images here
+// Import any new JSON files and code images here
 import run from "./katas/run.json";
 import runCodeImg from "./assets/run-code-screenshot.png";
 
 function App() {
+  // Add any new kata objects here
+  const katas = [
+    {
+      name: "Recursive run",
+      url: "/recursive-run",
+      json: run,
+      image: runCodeImg,
+    },
+  ];
+
   return (
     <div className="page-container">
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Add a new route below */}
-        <Route
-          path="/recursive-kata/:kata_id"
-          element={<Kata stepsJson={run} codeImage={runCodeImg} />}
-        />
+        <Route path="/" element={<Home katas={katas} />} />
+
+        {katas.map((kata, index) => (
+          <Route
+            key={index}
+            path={kata.url}
+            element={<Kata stepsJson={kata.json} codeImage={kata.image} />}
+          />
+        ))}
       </Routes>
     </div>
   );
