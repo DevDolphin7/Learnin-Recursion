f1a8727384e13e4245f3688613323278b1d42bfe
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-18 19:35:28 +0000

    Update img tag to correct alt text and remove inline styling

--- src/components/SidePane.jsx
+++ src/components/SidePane.jsx
@@ -4,14 +4,7 @@ function SidePane({ codeImage }) {
   return (
     <section className="side-pane-container">
       <section className="code-image">
-        <img
-          src={codeImage}
-          alt="A screenshot of a recursive function that counts down the number of kilometers left in a run"
-          style={{
-            maxWidth: "90%",
-            maxHeight: "90%",
-          }}
-        />
+        <img src={codeImage} alt="A screenshot of the code being analysed" />
       </section>
       <div className="call-stack-container">
         <CallStack />
