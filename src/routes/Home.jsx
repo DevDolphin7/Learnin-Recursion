93c5f38bfd3b77e44e9dcb108af8be7e0c7d993c
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-18 20:04:57 +0000

    Handle insufficient screen width, add license note for mark pro font

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,19 +1,49 @@
-import { Link } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import { SufficientScreenDimensionsContext } from "../contexts/SufficientScreenDimensions";
+import Header from "../components/Header";
+import KataList from "../components/KataList";
 
-function Home() {
+function Home({ katas }) {
+  const { sufficientDimensions, setSufficientDimensions } = useContext(
+    SufficientScreenDimensionsContext
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setSufficientDimensions(
+        window.innerWidth > 809 && window.innerHeight > 409
+      );
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="page-container">
-      <main>
-        <h1>Welcome to Learnin Recursion!</h1>
-        <p>Click a button below to get started:</p>
-        <Link to="/recursive-kata/1">
-          <button>Kata 1</button>
-        </Link>
-      </main>
-      <footer className="home-footer">
-        Created by Daniel Olver for Northcoders
-      </footer>
-    </div>
+    <>
+      <Header />
+      {sufficientDimensions ? (
+        <>
+          <main className="home-content-container">
+            <h1>Welcome to Learnin' Recursion!</h1>
+            <KataList katas={katas} />
+          </main>
+          <footer className="home-footer">
+            <p>
+              Icons made from{" "}
+              <a href="https://www.onlinewebfonts.com/icon">svg icons</a> is
+              licensed by CC BY 4.0
+            </p>
+            <p>Created by Dan Olver for Northcoders.</p>
+          </footer>
+        </>
+      ) : (
+        <h1 className="insufficient-screen-width">
+          The screen dimensions are too small to handle all the recursion!
+        </h1>
+      )}
+    </>
   );
 }
 
