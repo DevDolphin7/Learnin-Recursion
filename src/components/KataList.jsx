79977ac41518bdccb1a0b1a541d8171aca19cc95
DevDolphin7 <DevDolphin7@outlook.com>
2024-12-18 20:05:04 +0000

    Add component

---
+++ src/components/KataList.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function KataList({ katas }) {
+  const [codePreview, setCodePreview] = useState(null);
+
+  const handleHover = ({ target: { textContent } }) => {
+    katas.forEach((kata) => {
+      if (kata.name === textContent) {
+        setCodePreview(kata.image);
+      }
+    });
+  };
+
+  const handleRemoveHover = () => {
+    setCodePreview(null);
+  };
+
+  return (
+    <>
+      <div className="kata-list">
+        <ul>
+          {katas.map((kata, index) => (
+            <li key={index}>
+              <Link to={kata.url}>
+                <p onMouseOver={handleHover} onMouseOut={handleRemoveHover}>
+                  {kata.name}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {codePreview ? (
+        <div className="preview-image">
+          <img src={codePreview}></img>
+        </div>
+      ) : null}
+      ;
+    </>
+  );
+}
+
+export default KataList;
